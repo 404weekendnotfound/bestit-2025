a79f7d78d1e6635da67f6690c4ce4fc2d2ae3424
januszmaro <janusz.miro152@gmail.com>
2025-05-16 21:50:50 +0200

    update

--- bestit-fastapi/models.py
+++ bestit-fastapi/models.py
@@ -14,10 +14,10 @@ class User(SQLModel, table=True):
     linkedin: str | None = Field(default=None, index=True)
     age: int | None = Field(default=None, index=True)
 
-    job_experience: Optional[List["Job"]] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
-    interests: Optional[List["Interest"]] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
-    education: Optional[List["Education"]] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
-    certificates: Optional[List["Certificate"]] = Relationship(back_populates="user", sa_relationship_kwargs={"lazy": "selectin"})
+    job_experience: Optional[List["Job"]] = Relationship(back_populates="user", sa_relationship=Relationship(lazy="selectin"))
+    interests: Optional[List["Interest"]] = Relationship(back_populates="user", sa_relationship=Relationship(lazy="selectin"))
+    education: Optional[List["Education"]] = Relationship(back_populates="user", sa_relationship=Relationship(lazy="selectin"))
+    certificates: Optional[List["Certificate"]] = Relationship(back_populates="user", sa_relationship=Relationship(lazy="selectin"))
 
 
 class Job(SQLModel, table=True):
