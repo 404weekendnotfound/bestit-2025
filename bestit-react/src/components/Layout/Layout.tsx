d29e70ab658be460b8b4a2717bdc1f4e16081b41
januszmaro <janusz.miro152@gmail.com>
2025-05-17 01:05:17 +0200

    update

--- bestit-react/src/components/Layout/Layout.tsx
+++ bestit-react/src/components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div>
             <Nav />
-        <div className="layout">
+        <div className="container">
             <Sidebar />
             <main className="content">
                 {children}
