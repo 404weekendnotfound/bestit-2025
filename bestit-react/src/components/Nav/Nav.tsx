99d4a2b5a843d447e29055f0760ef17801954469
januszmaro <janusz.miro152@gmail.com>
2025-05-17 00:56:25 +0200

    added adminer

--- bestit-react/src/components/Nav/Nav.tsx
+++ bestit-react/src/components/Nav/Nav.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 const Nav = () => {
     return (
-        <nav>
+        <nav className="nav">
             <Link to="/" className="nav__logo" style={{textAlign: "center"}}>
                 {/* <img src={logo} alt="logo" /> */}
                 <h2>404 Weekend not found</h2>
