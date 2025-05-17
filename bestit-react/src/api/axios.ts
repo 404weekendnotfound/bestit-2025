9fc26ee0d89fa7244a4ab8935631090f66aa4bdf
januszmaro <janusz.miro152@gmail.com>
2025-05-17 07:54:26 +0200

    change api

--- bestit-react/src/api/axios.ts
+++ bestit-react/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = 'http://localhost:7562';
+export const API_URL = 'https://app.weekendnotfound.pl';
 
 const axiosInstance = axios.create({
     baseURL: API_URL
